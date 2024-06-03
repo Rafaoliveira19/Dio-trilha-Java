0adc2e28a64a0d8edf4caafad9f7534b593cfe79
rafael <Rafaeloliveira1344@gmail.com>
2024-06-03 02:26:59 -0300

    Criando a classe para exemplo de Operadores

---
+++ tipos-variaveis/src/TiposOperadores.java
@@ -0,0 +1,25 @@
+public class TiposOperadores {
+    public static void main(String[] args) {
+        String concatenacao =" ";
+
+        concatenacao = 1+1+1+"1";
+        System.out.print(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.print(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.print(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.print(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.print(concatenacao);
+
+    }
+}
